efc209b86bcc36687732af92f4a2e84a599cc5b6
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-02-05 03:40:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function fuzzyMatch(drivers, string) {
 }
 
 function matchName(drivers, word) {
-  return drivers.filter(driver => driver.word === word)
+  return drivers.filter(driver => (driver.word === word)
 }
