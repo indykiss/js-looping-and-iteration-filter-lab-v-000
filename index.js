3102c8aa13c2cea9866380f953face033ff2886b
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-02-05 03:36:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file
 
 function findMatching(drivers, word) {
-  drivers.filter(driver => driver.toLowerCase() == word.toLowerCase());
+  return drivers.filter(driver => driver.toLowerCase() == word.toLowerCase());
 }
 
 function fuzzyMatch() {}
